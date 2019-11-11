ef72dc796c96767e079933712826c4b10568510b
poxdose <570109232@qq.com>
2019-11-11 15:56:19 +0800

    kkk

--- src/main/java/com/qf/mapper/TeacherMapper.java
+++ src/main/java/com/qf/mapper/TeacherMapper.java
@@ -2,7 +2,14 @@ package com.qf.mapper;
 
 public interface TeacherMapper {
 
+    //老师修改密码
+    public int TeacherUpdateUpwd(String uname);
 
+
+    //周报打分
+    public int WeekPaperPoint(String title,int point);
+
+    //请假审批
 
 
 }
