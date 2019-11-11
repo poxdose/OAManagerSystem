c58b733cef28bbc4f485ccd9b70a0e30e46efcb7
dalichuqiji666 <623001672@qq.com>
2019-11-11 14:41:44 +0800

    配置项目

--- src/main/java/com/qf/App.java
+++ src/main/java/com/qf/App.java
@@ -1,13 +1,19 @@
 package com.qf;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
-public class App 
+@SpringBootApplication
+@MapperScan(value = "com.qf.mapper")
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class,args);
     }
 }
