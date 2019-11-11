7ec943539400eeb4f8d6eea53f400038a3a9aa84
YangFan-H <2682323804@qq.com>
2019-11-11 19:14:18 +0800

    管理员mapper

--- src/main/java/com/qf/App.java
+++ src/main/java/com/qf/App.java
@@ -1,13 +1,20 @@
 package com.qf;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
+@MapperScan(value = "com.qf.mapper")
 public class App 
 {
     public static void main( String[] args )
     {
+        SpringApplication.run(App.class,args);
         System.out.println( "Hello World!" );
     }
 }
--- src/main/java/com/qf/controller/a.java
+++ src/main/java/com/qf/controller/a.java
@@ -1,4 +1,12 @@
 package com.qf.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class a {
+    @RequestMapping("index")
+    public String index(){
+        return  "index";
+    }
 }
